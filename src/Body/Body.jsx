2f566ec4c68f5a65f4a7d73b50c58d07112f840c
Moukaila LABODJA <labodja.moukaila@gmail.com>
2020-03-13 10:36:42 +0000

    creation de la structure de projet

---
+++ src/Body/Body.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Body extends Component {
+    render() {
+        return (
+            <div>
+                Body
+            </div>
+        )
+    }
+}
+
+export default Body
