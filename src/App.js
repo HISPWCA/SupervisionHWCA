40889a8aae4dd8845491f385376d4bdebf9a7ad3
Moukaila LABODJA <labodja.moukaila@gmail.com>
2020-03-13 10:47:53 +0000

    mise a jour de la fonction App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Header from './Header/Header';
 import Body from './Body/Body';
 import Footer from './Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <Body />
+    <Footer />
+  </div>
+)
 
 export default App;
